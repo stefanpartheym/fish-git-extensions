793e0465ddc2c3ddd5a0cc34aee22f5b4084efd2
Stefan Partheymüller <s.partheymueller@initics.de>
2024-10-09 13:37:55 +0200

    gitext_checkout: use fzf to display branch list if available

--- functions/gitext_checkout.fish
+++ functions/gitext_checkout.fish
@@ -3,32 +3,42 @@ function gitext_checkout \
 
     set branches (git branch)
     if test (count $branches) -lt 2
-        echo "ERROR: No branches available for checkout."
+        echo "No branches available for checkout."
         return 1
     end
 
-    for branch in $branches
-        if test (string sub --length 1 "$branch") != "*"
-            set branch_list $branch_list (string sub -s 3 "$branch")
-        end
-    end
-
-    set branch_id 0
-    for branch_name in $branch_list
-        set branch_id (math $branch_id + 1)
-        echo "$branch_id: $branch_name"
-    end
-
-    while true
-        read -P "Switch to branch (enter 'q' to quit): " target_id
-        if test "$target_id" = "q"
-            break
-        else if string match --quiet --regex '\d' "$target_id" && test $target_id -ge 1 -a $target_id -le (count $branch_list)
-            git checkout $branch_list[$target_id]
-            break
+    if type -q fzf > /dev/null
+        set selection (git branch | grep -v "^\*" | fzf --height=20% --reverse --info=inline)
+        if test -z "$selection"
+            echo "No branch selected."
+            return 1
         else
-            echo "ERROR: Input '$target_id' is not valid"
+            set branch (string trim "$selection")
+            git checkout "$branch"
+        end
+    else
+        for branch in $branches
+            if test (string sub --length 1 "$branch") != "*"
+                set branch_list $branch_list (string sub -s 3 "$branch")
+            end
+        end
+
+        set branch_id 0
+        for branch_name in $branch_list
+            set branch_id (math $branch_id + 1)
+            echo "$branch_id: $branch_name"
+        end
+
+        while true
+            read -P "Switch to branch (enter 'q' to quit): " target_id
+            if test "$target_id" = "q"
+                break
+            else if string match --quiet --regex '\d' "$target_id" && test $target_id -ge 1 -a $target_id -le (count $branch_list)
+                git checkout $branch_list[$target_id]
+                break
+            else
+                echo "ERROR: Input '$target_id' is not valid"
+            end
         end
     end
-
 end
